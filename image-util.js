fddd2551e5d09ee767411a872e14c522f2a0a727
Daniel <mwdchang@gmail.com>
2020-01-28 23:19:12 -0500

    add blur filter

--- image-util.js
+++ image-util.js
@@ -35,6 +35,15 @@ class ImageUtil {
   }
 
 
+  static invert(data) {
+    const result = [];
+    for (let i = 0; i < data.length; i++) {
+      result.push(1 - data[i]);
+    }
+    return result;
+  }
+
+
   /**
    * Load image data into RGBA numeric array, which is
    * somewhat compatible with tensor data structure
@@ -72,4 +81,73 @@ class ImageUtil {
     });
     return imgRequest;
   }
+
+
+  // Filters.convolute = function(pixels, weights, opaque) {
+
+
+  // Adapted from https://www.html5rocks.com/en/tutorials/canvas/imagefilters/
+  static convolve(data, options, weights) {
+    const side = Math.round(Math.sqrt(weights.length));
+    const halfSide = Math.floor(side/2);
+    const sw = options.width;
+    const sh = options.height;
+    const channels = options.channels;
+
+    // pad output by the convolution matrix
+    var w = sw;
+    var h = sh;
+
+    const result = [];
+    // go through the destination image pixels
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        // calculate the weighed sum of the source image pixels that
+        // fall under the convolution matrix
+        let ch = [];
+        for (let c = 0; c < channels; c++) {
+          ch.push(0);
+        }
+
+        // Not well defined around edges
+        if (y - halfSide < 0 || y + halfSide >= h || x - halfSide < 0 || x + halfSide >= w) {
+          for (let c = 0; c < channels; c++) {
+            ch[c] = data[(y * w + x) * channels];
+          }
+        }
+
+        for (let cy = 0; cy < side; cy++) {
+          for (let cx = 0; cx < side; cx++) {
+            var scy = y + cy - halfSide;
+            var scx = x + cx - halfSide;
+            if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+              let srcOff = (scy * sw + scx) * channels;
+              let wt = weights[cy * side + cx];
+
+              for (let c = 0; c < channels; c++) {
+                ch[c] += data[srcOff + c] * wt;
+              }
+
+              /*
+              r += data[srcOff] * wt;
+              g += data[srcOff+1] * wt;
+              b += data[srcOff+2] * wt;
+              a += data[srcOff+3] * wt;
+              */
+            }
+          }
+        }
+        /*
+        dst[dstOff] = r;
+        dst[dstOff + 1] = g;
+        dst[dstOff + 2] = b;
+        dst[dstOff + 3] = a;
+        */
+        for (let c = 0; c < channels; c++) {
+          result.push(ch[c]);
+        }
+      }
+    }
+    return result;
+  };
 }
